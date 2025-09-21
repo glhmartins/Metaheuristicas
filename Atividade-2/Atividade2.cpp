780bb87b9efe948a5e18d4e864e3ee1b26a58e6f
glhmartins <glhmartinsrangel@gmail.com>
2025-09-21 19:35:22 -0300

    Modificações no first

--- Atividade-2/Atividade2.cpp
+++ Atividade-2/Atividade2.cpp
@@ -4,28 +4,28 @@
 void randomAscentMethod(int m, int max, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     s = construtorAleatorio(m, cap, s, d, b, ma);
     vector<int> fs = funcaoObjetivo(s, ma, d, b);
-    cout << "Beneficio pos construcao: " << fs[0] << " peso pos construcao: " << fs[1] << endl;
+    cout << "Beneficio pos construcao: " << fs[0] << endl << "peso pos construcao: " << fs[1] << endl;
     s = randomImprovement(m, max, cap, s, b, d, ma);
     fs = funcaoObjetivo(s, ma, d, b);
-    cout << "Beneficio pos busca randomizado: " << fs[0] << " peso pos busca randomizado: " << fs[1] << endl;
+    cout << "Beneficio pos busca randomizado: " << fs[0] << endl << "peso pos busca randomizado: " << fs[1] << endl;
 }
 
 void firstAscentMethod(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     s = construtorAleatorio(m, cap, s, d, b, ma);
     vector<int> fs = funcaoObjetivo(s, ma, d, b);
-    cout << "Beneficio pos construcao: " << fs[0] << " peso pos construcao: " << fs[1] << endl;
+    cout << "Beneficio pos construcao: " << fs[0] << endl << "peso pos construcao: " << fs[1] << endl;
     s = firstImprovement(m, cap, s, b, d, ma);
     fs = funcaoObjetivo(s, ma, d, b);
-    cout << "Beneficio pos busca first: " << fs[0] << " peso pos busca first: " << fs[1] << endl;
+    cout << "Beneficio pos busca first: " << fs[0] << endl << "peso pos busca first: " << fs[1] << endl;
 }
 
 void bestAscentMethod(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     s = construtorAleatorio(m, cap, s, d, b, ma);
     vector<int> fs = funcaoObjetivo(s, ma, d, b);
-    cout << "Beneficio pos construcao: " << fs[0] << " peso pos construcao: " << fs[1] << endl;
+    cout << "Beneficio pos construcao: " << fs[0] << endl << "peso pos construcao: " << fs[1] << endl;
     s = bestImprovement(m, cap, s, b, d, ma);
     fs = funcaoObjetivo(s, ma, d, b);
-    cout << "Beneficio pos busca best: " << fs[0] << " peso pos busca best: " << fs[1] << endl;
+    cout << "Beneficio pos busca best: " << fs[0] << endl << "peso pos busca best: " << fs[1] << endl;
 }
 
 int main(){
@@ -34,8 +34,8 @@ int main(){
     vector<vector<int>> ma;
     leArquivo(d, b, ma, "prob-software.txt",&m, &n, &ne, &cap);
     vector<int> p(m, 0);
-    randomAscentMethod(m, 25, cap, p, b, d, ma);
-    //firstAscentMethod(m, cap, p, b, d, ma);
+    //randomAscentMethod(m, 25, cap, p, b, d, ma);
+    firstAscentMethod(m, cap, p, b, d, ma);
     //bestAscentMethod(m, cap, p, b, d, ma);
     return 0;
 }
--- Atividade-3/buscaLocal.cpp
+++ Atividade-3/buscaLocal.cpp
@@ -4,12 +4,69 @@
 
 using namespace std;
 
-vector<int> randomStep(int m, int max, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
-    srand(time(NULL));
+vector<int> randomSwapStep(int m, int max, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
+    vector<int> s1;
+    vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
+    int r1, r2, i = 0, aux;
+    do {
+        s1 = s;
+        r1 = rand()%s.size();
+        r2 = rand()%s.size();
+        if (r1!=r2){
+            aux = s1[r1];
+            s1[r1] = s1[r2];
+            s1[r2] = aux;
+            fn = funcaoObjetivo(s1, ma, d, b);
+            if((fn[0]>fs[0]) && (fn[1]<=cap)) return s1;
+        }
+        i++;
+    } while (i<max);
+    return s;
+}
+
+vector<int> firstSwapStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
+    vector<int> s1;
+    vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
+    int aux;
+    for(int i = 0; i<m-1; i++){
+        for(int j = i+1; j<m; j++){
+            s1 = s;
+            aux = s1[i];
+            s1[i] = s1[j];
+            s1[j] = aux; 
+            fn = funcaoObjetivo(s1, ma, d, b);
+            if((fn[0]>fs[0]) && (fn[1]<=cap)) return s1;
+        }
+    }
+    return s;
+}
+
+vector<int> bestSwapStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
+    vector<int> s1;
+    vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
+    int aux;
+    for(int i = 0; i<m-1; i++){
+        for(int j = i+1; j<m; j++){
+            s1 = s;
+            aux = s1[i];
+            s1[i] = s1[j];
+            s1[j] = aux; 
+            fn = funcaoObjetivo(s1, ma, d, b);
+            if((fn[0]>fs[0]) && (fn[1]<=cap)) {
+                fs = fn;
+                s = s1;
+            }
+        }
+    }
+    return s;
+}
+
+vector<int> randomFlipStep(int m, int max, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     vector<int> s1 = s;
     vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
     int r, i = 0;
     do {
+        s1 = s;
         r = rand()%s.size();
         s1[r] = (s1[r] == 1) ? 0:1; 
         fn = funcaoObjetivo(s1, ma, d, b);
@@ -19,7 +76,7 @@ vector<int> randomStep(int m, int max, int cap, vector<int> s, vector<int> b, ve
     return s;
 }
 
-vector<int> firstStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
+vector<int> firstFlipStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     vector<int> s1;
     vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
     for(int i = 0; i<m; i++){
@@ -31,7 +88,7 @@ vector<int> firstStep(int m, int cap, vector<int> s, vector<int> b, vector<int> 
     return s;
 }
 
-vector<int> bestStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
+vector<int> bestFlipStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     vector<int> s1;
     vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
     for(int i = 0; i<m; i++){
@@ -48,13 +105,16 @@ vector<int> bestStep(int m, int cap, vector<int> s, vector<int> b, vector<int> d
 
 vector<int> randomImprovement(int m, int max, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
     srand(time(NULL));
-    vector<int> s1 = s;
+    vector<int> s1 = s, s2 = s, sf;
     vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
-    int i = 0;
+    int i = 0, r;
     do {
-        s1 = randomStep(m, max, cap, s, b, d, ma);
-        if(s!=s1){
-            s = s1;
+        r = rand()%2;
+        s1 = randomSwapStep(m, max, cap, s, b, d, ma);
+        s2 = randomFlipStep(m, max, cap, s, b, d, ma);
+        sf = (r == 0) ? s1:s2;
+        if(s!=sf){
+            s = sf;
             i = 0;
         }
         i++;
@@ -63,13 +123,15 @@ vector<int> randomImprovement(int m, int max, int cap, vector<int> s, vector<int
 }
 
 vector<int> firstImprovement(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
-    vector<int> s1 = s;
+    vector<int> s1 = s, s2 = s, sf;
     vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
     int i = 0;
     do {
-        s1 = firstStep(m, cap, s, b, d, ma);
-        if(s!=s1){
-            s = s1;
+        s1 = firstSwapStep(m, cap, s, b, d, ma);
+        s2 = firstFlipStep(m, cap, s, b, d, ma);
+        sf = (funcaoObjetivo(s1, ma, d, b)>funcaoObjetivo(s2, ma, d, b)) ? s1:s2;
+        if(s!=sf){
+            s = sf;
             i = 0;
         }
         else break;
@@ -79,13 +141,15 @@ vector<int> firstImprovement(int m, int cap, vector<int> s, vector<int> b, vecto
 }
 
 vector<int> bestImprovement(int m, int cap, vector<int> s, vector<int> b, vector<int> d, vector<vector<int>> ma){
-    vector<int> s1 = s;
+    vector<int> s1 = s, s2 = s, sf;
     vector<int> fs = funcaoObjetivo(s, ma, d, b), fn;
     int i = 0;
     do {
-        s1 = bestStep(m, cap, s, b, d, ma);
-        if(s!=s1){
-            s = s1;
+        s1 = bestSwapStep(m, cap, s, b, d, ma);
+        s2 = bestFlipStep(m, cap, s, b, d, ma);
+        sf = (funcaoObjetivo(s1, ma, d, b)>funcaoObjetivo(s2, ma, d, b)) ? s1:s2;
+        if(sf!=s){
+            s = sf;
             i = 0;
         }
         else break;
